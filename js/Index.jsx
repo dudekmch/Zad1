83df41dd9ab249be8c1e1fa8195c89d2db4eafe9
P.Dudek <paweldominikdudek@gmil.com>
2018-06-05 19:06:44 +0200

    Upgrade react and react-dom version to 16.3

--- js/Index.jsx
+++ js/Index.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ClientApp from "./ClientApp";
+import FormContext from "./FormContext"
 
 const App = () => (
   <BrowserRouter>
+  <FormContext>
     <div className="app">
       <Switch>
         <Route exact path="/" component={ClientApp} />
       </Switch>
     </div>
+    </FormContext>
   </BrowserRouter>
 );
 
